fb48426efcd7df77c256d0a2534c999bbe567cc9
xiejixiong <1053406770@qq.com>
2019-12-13 16:07:46 +0800

    111

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// 引入React
+import React from 'react'
+import './App.css'
+import Drawers from "./pages/Drawer/Drawer.jsx"
+
+export default class App extends React.Component {
+
+  render () {
+    return (
+      <Drawers />
+    )
+  }
+}
---
+++ src/pages/Grabble/Grabble.jsx
@@ -0,0 +1,53 @@
+// 引入React
+import React from 'react'
+import './Grabble.css'
+import { InputItem, Icon } from 'antd-mobile';
+export default class Grabble extends React.Component {
+  state = {
+    isHeadMasking: true
+  }
+  goMasking = () => {
+    const headMasking = document.getElementById("headMasking")
+    if (this.state.isHeadMasking) {
+      this.setState({
+        isHeadMasking: false
+      })
+      headMasking.className = "headMasking isHeadMasking"
+    }
+  }
+  goGrabble = () => {
+    const headMasking = document.getElementById("headMasking")
+    if (!this.state.isHeadMasking) {
+      this.setState({
+        isHeadMasking: true
+      })
+      headMasking.className = "headMasking"
+    }
+  }
+  render () {
+    return (
+      <div>
+        <div className="headBottom" onClick={this.goMasking}>
+          <div className="search">
+            <img src="./image/icon-search-default.png" alt="" />
+            <span className="text">搜索</span>
+          </div>
+        </div>
+        <div id="headMasking" className="headMasking">
+          <div>
+            <div className="headMaskingTop">
+              <Icon type="left" size="lg" onClick={this.goGrabble} />
+              <div className="headMaskingTopInput">
+                <InputItem
+                  clear
+                  placeholder="支持音乐搜索"
+                ></InputItem>
+              </div>
+              <span>搜索</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
